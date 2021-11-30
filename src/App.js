fd64ad2add8017a1ddceb3ef13111cc38882f69c
abc <abc@example.com>
2021-11-30 12:34:48 -0700

    I am getting data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import axios from 'axios'
 
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/mattv731')
+  .then(resp => {
+    console.log(resp.data)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+}
   render() {
     return(<div>
       Github Card
